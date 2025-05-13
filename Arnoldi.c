63ed494e3066bbd0a7787f6196ffcb5f17fc8639
Lorenzo <153271170+lorematta@users.noreply.github.com>
2025-05-13 17:54:01 +0200

    Update Arnoldi.c

--- Arnoldi.c
+++ Arnoldi.c
@@ -30,13 +30,35 @@ void Arnoldi (double A[N][N], double v[N]){
     }
     
     for (int i = 0; i < P-2; i++){
-        matvec(A, v, r);
-        matvec(V, r, h);
+        double H [i][i];
+        double H_new [i+1][i+1];
+        matvec(A, v, r);  // r = A*v_k  dim N
+        mattrasp(N, V);   // V^T  
+        matvec(V, r, h);  // h = V^T *r  dim P+1
+        mattrasp(N, V);   // V  
+        double y [P+1];
+        matvec(V,h,y); // r = r - V*h  dim N
+
+        for (int j = 0; i<N ; i++){
+            r[j] -= y[j];
+        }
+
+        b = norma(r);
+        int flag1 = 0;
+        int flag2 = 0; 
+        for (int j = 0; j<i; j++){
+            for (int k = 0; k<j; k++){
+                H_new[j][k] =  H[j][k];
+                flag1 = k;    
+            }
+            H_new[j][flag1+1] = h[j];
+            flag2 = j;
+        }
+        H_new[flag2+1][0] = b;
+        
         
     }
     
-    
-
     
 }
 

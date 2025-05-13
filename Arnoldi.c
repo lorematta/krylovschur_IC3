6c88538db91a4b8b5e59de2b1a3efee182438b31
Lorenzo <153271170+lorematta@users.noreply.github.com>
2025-05-13 17:03:51 +0200

    Update Arnoldi.c

--- Arnoldi.c
+++ Arnoldi.c
@@ -1,21 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <Funzioni.h>  //mettere #include "Funzioni.h" per cercare prima nella directory locale del progetto
+#include "Funzioni.h" // in xcode non serve mettere #include, rispetto a vscode usa "" e non <>
 
 #define N 4  //dim mat A
 #define P 3  //numero passi Arnoldi
 
 void Arnoldi (double A[N][N], double v[N]){
 
+
+    
     if (P>N){
-        printf("number of steps can't be bigger than space dimension");
-        exit;
+        printf("number of steps can't be bigger than space dimension"); // fprintf stamperebbe su *FILE generico e non solo su stdoutput
+        exit(EXIT_FAILURE);
+    }else if (norma(v) != 1){
+        printf("input vector is not a unitary vector");
+        exit(EXIT_FAILURE);
     }
+    
+    double H[P+1][P];
+    double V[N][P+1];
+    double r[N];
+    double h[N];
+    
+    for(int i=0; i<N; i++){
+        for (int j = 0; j<N; j++){
+            V[j][i] = v[i];
+        }
+    }
+    
+    for (int i = 0; i < P-2; i++){
+        matvec(A, v, r);
+        matvec(V, r, h);
+        
+    }
+    
+    
 
     
 }
-
 
 
 

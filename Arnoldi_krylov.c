3e579eca5ea9ae713bb16ca641d73e7871eddc3d
Lorenzo <153271170+lorematta@users.noreply.github.com>
2025-05-13 14:08:28 +0200

    Update and rename main_krylov.c to Arnoldi_krylov.c

---
+++ Arnoldi_krylov.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <Funzioni.h>
+
+#define N 4  //dim mat A
+#define P 3  //numero passi Arnoldi
+
+void Arnoldi (double A[N][N], double v[N]){
+
+    if (P>N){
+        printf("number of steps can't be bigger than space dimension");
+        exit;
+    }
+
+    
+}
+
+
+
+
+
+

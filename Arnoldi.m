506b85db71eeab2c402e85413294ae1c3202eb1f
Lorenzo <153271170+lorematta@users.noreply.github.com>
2025-05-20 16:41:16 +0200

    Update Arnoldi.m

--- Arnoldi.m
+++ Arnoldi.m
@@ -1,6 +1,7 @@
 function [V, H, r, b] = Arnoldi(v,A, m)
 
-if norm(v) ~= 1
+Toll = 1e-3;
+if abs(norm(v) - 1) > Toll
     error("initial vector with no unitary norm");
 end
 
@@ -20,6 +21,9 @@ for i = 1:m-1
     h = V'*r;
     r = r - V*h; % se a questo punto si crea r = 0 signfica che il sottospazio di krylov ha raggiunto la sua
                  % massima dimensione 
+
+                 % si osserva che da qui non c'è una perfetta
+                 % ortogonalizzazione
     b = norm(r);
 
     if b < tol
@@ -33,7 +37,10 @@ for i = 1:m-1
     H = [H, h; zeros(1,i), b];
     end
     v = r/b;
-    V (:,i+1) = v;
+    V (:,i+1) = v;   % attenzione, V è generalmente rettangolare (a meno che il sottospazio di krylov non coincida 
+                     % con lo spazio di partenza quindi V'V è identità
+                     % ma V V' no
+                     
 end
 r = A*v;
 h = V'*r;

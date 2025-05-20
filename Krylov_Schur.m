976451fd0d025bc0dae4dfb980cf448f5c964b1e
Lorenzo <153271170+lorematta@users.noreply.github.com>
2025-05-20 16:41:50 +0200

    Update Krylov_Schur.m

--- Krylov_Schur.m
+++ Krylov_Schur.m
@@ -1,66 +1,71 @@
-function V = Krylov_Schur(v,A,m,k)
+function [V,it] = Krylov_Schur(v,A,m,k)
 
 res = 1;
 it = 0;
 V = v;
 while (res > 1e-5 && it< 500)
 
-V = Arnoldi(V(:,1),A,m);
+V = Arnoldi(V(:,1)/norm(V(:,1)),A,m);
 % schur utilizza l'output di uscita di Arnoldi come restart se gli
 % autovalori non sono convergenti 
 
-lw = []; %autovalori cercati
-lu = []; % autovalori unwanted
-B = [];
 Hsq = V'*A*V; %Matrice quadrata di Heissemberg
-[y,th] = eig(Hsq); 
-resid = zeros(m,1);
+% [y,th] = eig(Hsq); 
+% resid = zeros(m,1);
 
 
-%verifica convergenza arnoldi
-for i=1:m
-
-    yi = y(:,i);
-    xi = V*yi;
-    li = th(i,i);
-    resid(i) = norm(A*xi-li*xi);
-
-end
-
-tol = 1e-2;
-if norm(resid(end)-resid(1)) < tol
-    error("autovalori convergenti, usare direttamente arnoldi");
-end
+% %verifica convergenza arnoldi
+% for i=1:m
+% 
+%     yi = y(:,i);
+%     xi = V*yi;
+%     li = th(i,i);
+%     resid(i) = norm(A*xi-li*xi);
+% 
+% end
+% 
+% tol = 1e-2;
+% if norm(resid(end)-resid(1)) < tol
+%     error("autovalori convergenti, usare direttamente arnoldi");
+% end
 
 [Q,T] = schur(Hsq);
 lt = eig(T);
-wanted = real(lt)>0;
-p = sum(wanted);
+[~,idx] = sort(lt,"descend");
+re = real(lt)>0;
+p = sum(re);
 
 if p == 0
     disp("Nessun autovalore wanted trovato.");
     return;
 end 
 
-[Q_ord, T_ord] = ordschur(Q,T,wanted);   % Riordina la decomposizione di Schur in wanted e unwanted
+wanted = false(length(lt),1);
+mod = false(length(lt),1);
 
-
-if p>k
-    Bw = T_ord(1:k,1:k);
-    Qw = Q_ord(:, 1:k);
-else
-    Bw = T_ord(1:p,1:p);
-    Qw = Q_ord(:, 1:p);
+for i=1:length(lt)
+    mod(idx(i)) = true;
 end
 
+for i= 1:length(lt)
+    if mod(i) && re(i)
+        wanted(i) = 1;
+    end
+end
 
- V = V * Qw;       % nuova base ortonormale (n x k)
+[Q_ord, T_ord] = ordschur(Q,T,wanted);   % Riordina la decomposizione di Schur in wanted e unwanted
+
+Bw = T_ord(1:p,1:p);
+Qw = Q_ord(:, 1:p);
+
+
+ V = V * Qw; 
+ [V, ~] = qr(V,0); % nuova base ortonormale (n x k)
  Hsq = V'*A*V; %Matrice quadrata di Heissemberg
 [y,th] = eig(Hsq); 
 resid = zeros(k,1);
 
-
-%verifica convergenza arnoldi
+%verifica convergenza
 for i=1:k
 
     yi = y(:,i);
@@ -69,6 +74,11 @@ for i=1:k
     resid(i) = norm(A*xi-li*xi);
 
 end
-res =  norm(resid(end)-resid(1));
-it = it+1;
+res = max(resid);
+it = it+1
+end
+
+
+Qw = Q_ord(:, 1:k);
+V = V * Qw;
 end
